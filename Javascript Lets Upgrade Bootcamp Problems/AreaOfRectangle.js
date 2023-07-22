55e802202f9f7d448b60503ce2baf52bc67825ae
Vijay-Anandharaman <anantharamanvijay@gmail.com>
2023-07-22 19:43:02 +0530

    Javascript Problems

---
+++ Javascript Lets Upgrade Bootcamp Problems/AreaOfRectangle.js
@@ -0,0 +1,12 @@
+var width = 5;
+
+var length = 9;
+
+function findAreaOfRectangle(width, length) 
+ {
+    return width * length;
+ }
+
+var areaOfRectangle = findAreaOfRectangle(width, length);
+
+console.log("Area of rectangle : " + areaOfRectangle);
